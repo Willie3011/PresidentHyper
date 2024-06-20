93e8a308308a4aa483f1adce4f98146d5ccf038e
Buddynice34 <willemapril33@gmail.com>
2024-06-20 13:08:33 +0200

    bug: made header fixed to top

--- JS/data.js
+++ JS/data.js
@@ -19,6 +19,31 @@ const Categories = [
         name: "Bakery",
         imageUrl: "../assets/bread and bakery.jpg"
     },
+    {
+        id: 5,
+        name: "Bakery",
+        imageUrl: "../assets/bread and bakery.jpg"
+    },
+    {
+        id: 6,
+        name: "Bakery",
+        imageUrl: "../assets/bread and bakery.jpg"
+    },
+    {
+        id: 7,
+        name: "Bakery",
+        imageUrl: "../assets/bread and bakery.jpg"
+    },
+]
+
+const Products = [
+    {
+        id: 1,
+        name: "Name",
+        description: "Description", 
+        price: 12.99,
+        rating: 3.5
+    }
 ]
 
 export default Categories;
