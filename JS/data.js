718fc746531b2e45564f56dc3e16b4e982756864
Buddynice34 <willemapril33@gmail.com>
2024-06-21 18:21:48 +0200

    bug: images fix v1

--- JS/data.js
+++ JS/data.js
@@ -2,95 +2,95 @@ export const Categories = [
     {
         id: 1,
         name: "Fruits",
-        imageUrl: "../assets/fruits.jpg"
+        imageUrl: "/../assets/fruits.jpg"
     },
     {
         id: 2,
         name: "Vegetables",
-        imageUrl: "../assets/veges-cat.jpg"
+        imageUrl: "/../assets/veges-cat.jpg"
     },
     {
         id: 3,
         name: "Soft Drinks",
-        imageUrl: "../assets/soft-drinks.jpg"
+        imageUrl: "/../assets/soft-drinks.jpg"
     },
     {
         id: 4,
         name: "Bakery",
-        imageUrl: "../assets/bread and bakery.jpg"
+        imageUrl: "/../assets/bread and bakery.jpg"
     }
 ]
 
 export const dailyProducts = [
     {
         id: 1,
-        imageUrl: '../assets/bread.webp',
+        imageUrl: '/../assets/bread.webp',
         name: "SASKO Premium Sliced White Bread 700g", 
         price: "18.99"
     },
     {
         id: 2,
-        imageUrl: '../assets/bananas.webp',
+        imageUrl: '/../assets/bananas.webp',
         name: "Bananas Bag 750g", 
         price: "18.99"
     },
     {
         id: 3,
-        imageUrl: '../assets/milk.webp',
+        imageUrl: '/../assets/milk.webp',
         name: "Douglasdale Fresh Full Cream Milk Bottle 2L", 
         price: "27.99"
     },
     {
         id: 4,
-        imageUrl: '../assets/mandarins.webp',
+        imageUrl: '/../assets/mandarins.webp',
         name: "Mandarins Bag 1kg", 
         price: "21.99"
     },
     {
         id: 5,
-        imageUrl: '../assets/six pack milk.webp',
+        imageUrl: '/../assets/six pack milk.webp',
         name: "Parmalat Everfresh UHT Full Craem Milk Pack 6 x 1L", 
         price: "99.99"
     },
     {
         id: 6,
-        imageUrl: '../assets/crunchie.webp',
+        imageUrl: '/../assets/crunchie.webp',
         name: "Cadbury Crunchie Chocolate Bar 40g", 
         price: "13.99"
     },
     {
         id: 6,
-        imageUrl: '../assets/crunchie.webp',
+        imageUrl: '/../assets/crunchie.webp',
         name: "Cadbury Crunchie Chocolate Bar 40g", 
         price: "13.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/lays.webp',
+        imageUrl: '/../assets/lays.webp',
         name: "Lays Salted Potato Chips 120g", 
         price: "18.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/lays.webp',
+        imageUrl: '/../assets/lays.webp',
         name: "Lays Salted Potato Chips 120g", 
         price: "18.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/lays.webp',
+        imageUrl: '/../assets/lays.webp',
         name: "Lays Salted Potato Chips 120g", 
         price: "18.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/lays.webp',
+        imageUrl: '/../assets/lays.webp',
         name: "Lays Salted Potato Chips 120g", 
         price: "18.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/lays.webp',
+        imageUrl: '/../assets/lays.webp',
         name: "Lays Salted Potato Chips 120g", 
         price: "18.99"
     },
@@ -100,73 +100,73 @@ export const dailyProducts = [
 export const bakeryProducts = [
     {
         id: 1,
-        imageUrl: '../assets/bakery/albany.webp',
+        imageUrl: '/../assets/bakery/albany.webp',
         name: "Albany Superior Sliced Brown Bread Loaf 700g",
         price: "14.99"
     },
     {
         id: 2,
-        imageUrl: '../assets/bakery/sunbake.webp',
+        imageUrl: '/../assets/bakery/sunbake.webp',
         name: "Sunbake White Bread Loaf 700g",
         price: "18.99"
     },
     {
         id: 3,
-        imageUrl: '../assets/bakery/blueribbon.webp',
+        imageUrl: '/../assets/bakery/blueribbon.webp',
         name: "Blue Ribbon Classic White Bread Loaf 700g",
         price: "19.99"
     },
     {
         id: 4,
-        imageUrl: '../assets/bakery/saskoWhite.webp',
+        imageUrl: '/../assets/bakery/saskoWhite.webp',
         name: "SASKO Premium Sliced White Bread Loaf 700g",
         price: "18.99"
     },
     {
         id: 5,
-        imageUrl: '../assets/bakery/saskoBrown.webp',
+        imageUrl: '/../assets/bakery/saskoBrown.webp',
         name: "SASKO Premium Sliced Brown Bread Loaf 700g",
         price: "17.99"
     },
     {
         id: 6,
-        imageUrl: '../assets/bakery/albanyWhite.webp',
+        imageUrl: '/../assets/bakery/albanyWhite.webp',
         name: "Albany Superior Sliced White Bread Loaf 700g",
         price: "13.99"
     },
     {
         id: 7,
-        imageUrl: '../assets/bakery/rolls.webp',
+        imageUrl: '/../assets/bakery/rolls.webp',
         name: "Bakery White Hamburger Rolls 6 Pack",
         price: "16.99"
     },
     {
         id: 9,
-        imageUrl: '../assets/bakery/blueBrown.webp',
+        imageUrl: '/../assets/bakery/blueBrown.webp',
         name: "Blue Ribbon Classic Brown Bread Loaf 700g",
         price: "14.99"
     },
     {
         id: 10,
-        imageUrl: '../assets/bakery/sunbake hotdogs.webp',
+        imageUrl: '/../assets/bakery/sunbake hotdogs.webp',
         name: "Sunbake White Hotdog Rolls 6 Pack",
         price: "21.99"
     },
     {
         id: 11,
-        imageUrl: '../assets/bakery/buns.webp',
+        imageUrl: '/../assets/bakery/buns.webp',
         name: "SASKO Regular White Buns 6 Pack",
         price: "19.99"
     },
     {
         id: 12,
-        imageUrl: '../assets/bakery/hotdog rolls.webp',
+        imageUrl: '/../assets/bakery/hotdog rolls.webp',
         name: "Albany Hotdog Brown Rolls 6 pack",
         price: "21.99"
     },
     {
         id: 13,
-        imageUrl: '../assets/bakery/rollsBrown.webp',
+        imageUrl: '/../assets/bakery/rollsBrown.webp',
         name: "Brown Hamburger Rolls 6 pack",
         price: "12.99"
     },
